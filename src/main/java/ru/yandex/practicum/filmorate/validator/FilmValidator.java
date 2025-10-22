4fa263ab3d696699866fba2f9f772eb36b0b8692
bistrica.iurie <bistrica.iurie95@gmail.com>
2025-10-22 17:21:05 +0300

    10 Sptint solution

--- src/main/java/ru/yandex/practicum/filmorate/validator/FilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/FilmValidator.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Slf4j
