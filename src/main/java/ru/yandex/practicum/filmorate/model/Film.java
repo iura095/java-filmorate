8534179111ba8ca13f7f73f642e54526ed6c4033
bistrica.iurie <bistrica.iurie95@gmail.com>
2025-10-24 09:52:23 +0300

    10 Sptint solution

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDate;
 
@@ -16,6 +21,16 @@ public class Film {
     private String name;
     private String description;
     private LocalDate releaseDate;
+    @JsonSerialize(using = DurationToMinutesSerializer.class)
     private Duration duration;
 
+}
+
+class DurationToMinutesSerializer extends JsonSerializer<Duration> {
+
+    @Override
+    public void serialize(Duration value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        long minutes = value.toMinutes();
+        gen.writeNumber(minutes);
+    }
 }
